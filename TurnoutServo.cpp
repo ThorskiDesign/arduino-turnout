f0294cde40119b6deba8f3d6b19d9b411de7f539
Eric Thorstenson <eric@thorski.com>
2017-04-17 14:54:18 -0400

    Add typedef for callback functions. Add Initialization for vars that didn't have it.

--- TurnoutServo.cpp
+++ TurnoutServo.cpp
@@ -5,36 +5,36 @@
 // Create a new TurnoutServo
 TurnoutServo::TurnoutServo(byte ServoPin, byte PowerPin)
 {
-	servoPin = ServoPin;
-	powerPin = PowerPin;
+    servoPin = ServoPin;
+    powerPin = PowerPin;
 }
 
 
 // Initialize the TurnoutServo (e.g., with values read from eeprom for extents and last position)
 void TurnoutServo::Initialize(byte ExtentLow, byte ExtentHigh, bool Position)
 {
-	pinMode(powerPin, OUTPUT);
-	extent[LOW] = ExtentLow;
-	extent[HIGH] = ExtentHigh;
-	positionSet = Position;
+    pinMode(powerPin, OUTPUT);
+    extent[LOW] = ExtentLow;
+    extent[HIGH] = ExtentHigh;
+    positionSet = Position;
 
-	// update steps and intervals
-	ComputeSteps();
+    // update steps and intervals
+    ComputeSteps();
 }
 
 
 // Initialize the TurnoutServo (e.g., with values read from eeprom for extents, rates, and last position)
 void TurnoutServo::Initialize(byte ExtentLow, byte ExtentHigh, int DurationLow, int DurationHigh, bool Position)
 {
-	pinMode(powerPin, OUTPUT);
-	extent[LOW] = ExtentLow;
-	extent[HIGH] = ExtentHigh;
-	duration[LOW] = DurationLow;
-	duration[HIGH] = DurationHigh;
-	positionSet = Position;
+    pinMode(powerPin, OUTPUT);
+    extent[LOW] = ExtentLow;
+    extent[HIGH] = ExtentHigh;
+    duration[LOW] = DurationLow;
+    duration[HIGH] = DurationHigh;
+    positionSet = Position;
 
-	// update steps and intervals
-	ComputeSteps();
+    // update steps and intervals
+    ComputeSteps();
 }
 
 
@@ -42,51 +42,51 @@ void TurnoutServo::Initialize(byte ExtentLow, byte ExtentHigh, int DurationLow, 
 // Power off servo after duration plus delay
 void TurnoutServo::Update(unsigned long CurrentMillis)
 {
-	// if we're off, just return
-	if (servoState == OFF) return;
+    // if we're off, just return
+    if (servoState == OFF) return;
 
-	// if we're starting, check the time and power on when needed
-	if (servoState == STARTING && CurrentMillis > startTime)
-	{
-		digitalWrite(powerPin, HIGH);
-		servoState = MOVING;          // advance to next state
-		return;
-	}
+    // if we're starting, check the time and power on when needed
+    if (servoState == STARTING && CurrentMillis > startTime)
+    {
+        digitalWrite(powerPin, HIGH);
+        servoState = MOVING;          // advance to next state
+        return;
+    }
 
-	// if we're moving, compute the steps needed
-	if (servoState == MOVING)
-	{
-		// check if step interval for the given rate has elapsed
-		if (CurrentMillis > lastUpdate)
-		{
-			lastUpdate = CurrentMillis + interval[rateSet];
+    // if we're moving, compute the steps needed
+    if (servoState == MOVING)
+    {
+        // check if step interval for the given rate has elapsed
+        if (CurrentMillis > lastUpdate)
+        {
+            lastUpdate = CurrentMillis + interval[rateSet];
 
-			// if we still have steps to go in this movement
-			if (currentStep < numSteps)
-			{
-				write(steps[positionSet][currentStep]);      // set servo to new position
-				currentStep++;
-			}
-			else
-			{
-				currentStep = 0;           // reset counter for next movement
-				servoState = STOPPING;     // advance to next state
-				if (servoMoveDoneHandler) servoMoveDoneHandler();    // raise event indicating servo motion is complete
-			}
-		}
-		return;
-	}
+            // if we still have steps to go in this movement
+            if (currentStep < numSteps)
+            {
+                write(steps[positionSet][currentStep]);      // set servo to new position
+                currentStep++;
+            }
+            else
+            {
+                currentStep = 0;           // reset counter for next movement
+                servoState = STOPPING;     // advance to next state
+                if (servoMoveDoneHandler) servoMoveDoneHandler();    // raise event indicating servo motion is complete
+            }
+        }
+        return;
+    }
 
-	// if we're waiting to stop, check time and power off if needed
-	if (servoState == STOPPING && CurrentMillis > stopTime)
-	{
-		servoState = OFF;                    // reset to OFF state
-		digitalWrite(powerPin, LOW);         // disable the servo power
-		detach();                            // stop sending pwm pulses
+    // if we're waiting to stop, check time and power off if needed
+    if (servoState == STOPPING && CurrentMillis > stopTime)
+    {
+        servoState = OFF;                    // reset to OFF state
+        digitalWrite(powerPin, LOW);         // disable the servo power
+        detach();                            // stop sending pwm pulses
 
-		// raise the event indicating the servo has been powered off
-		if (servoPowerOffHandler) servoPowerOffHandler();
-	}
+        // raise the event indicating the servo has been powered off
+        if (servoPowerOffHandler) servoPowerOffHandler();
+    }
 }
 
 
@@ -105,50 +105,50 @@ bool TurnoutServo::IsActive() { return (servoState != OFF); }
 // Sets the servo to a position at the specified rate
 void TurnoutServo::Set(bool Position, bool Rate)
 {
-	// if we aren't on already, and position is new
-	if (servoState == OFF && Position != positionSet)
-	{
-		// ensure we are sending pulses for the current position
-		attach(servoPin);
-		write(extent[positionSet]);
+    // if we aren't on already, and position is new
+    if (servoState == OFF && Position != positionSet)
+    {
+        // ensure we are sending pulses for the current position
+        attach(servoPin);
+        write(extent[positionSet]);
 
-		// update position and rate settings
-		positionSet = Position;
-		rateSet = Rate;
+        // update position and rate settings
+        positionSet = Position;
+        rateSet = Rate;
 
-		// set state and start/stop times
-		servoState = STARTING;
-		startTime = millis() + servoStartDelay;
-		stopTime = millis() + duration[rateSet] + servoStopDelay;
+        // set state and start/stop times
+        servoState = STARTING;
+        startTime = millis() + servoStartDelay;
+        stopTime = millis() + duration[rateSet] + servoStopDelay;
 
-		// get the movement range from the current position to the set position 
-		movementRange = extent[positionSet] - extent[!positionSet];
-	}
+        // get the movement range from the current position to the set position 
+        movementRange = extent[positionSet] - extent[!positionSet];
+    }
 }
 
 
 // Set an extent for the servo (e.g., while adjusting turnout endpoints)
 void TurnoutServo::SetExtent(bool Position, byte Extent)
 { 
-	// set the new value
-	extent[Position] = Extent;
+    // set the new value
+    extent[Position] = Extent;
 
-	// update steps and intervals
-	ComputeSteps();
+    // update steps and intervals
+    ComputeSteps();
 
-	// adjust the current position of the servo if we're in that position
-	if (Position == HIGH && positionSet == HIGH) Set(HIGH, HIGH);
-	if (Position == LOW && positionSet == LOW) Set(LOW, HIGH);
+    // adjust the current position of the servo if we're in that position
+    if (Position == HIGH && positionSet == HIGH) Set(HIGH, HIGH);
+    if (Position == LOW && positionSet == LOW) Set(LOW, HIGH);
 }
 
 
 // Set the duration for slow/fast rate
 void TurnoutServo::SetDuration(bool Position, int Duration)
 {
-	duration[Position] = Duration;
+    duration[Position] = Duration;
 
-	// update intervals
-	ComputeSteps();
+    // update intervals
+    ComputeSteps();
 }
 
 
@@ -156,20 +156,23 @@ void TurnoutServo::SetDuration(bool Position, int Duration)
 // set the timing interval for low and high rates (note may have small error in total elapsed time due to truncation)
 void TurnoutServo::ComputeSteps()
 {
-	for (int i=0; i<numSteps; i++)
-	{
-		int movementRange = extent[1] - extent[0];              // total range of movement
-		int increment = ((float)(i+1)/numSteps)*movementRange;  // increment for each step (force floating point division)
-		steps[0][i] = extent[1]-increment;                      // steps going from high to low
-		steps[1][i] = extent[0]+increment;                      // steps going from low to high
-		//Serial.print("low = ");
-		//Serial.print(steps[0][i],DEC);
-		//Serial.print("   high = ");
-		//Serial.println(steps[1][i],DEC);
-	}
+    for (int i=0; i<numSteps; i++)
+    {
+        int movementRange = extent[1] - extent[0];              // total range of movement
+        int increment = ((float)(i+1)/numSteps)*movementRange;  // increment for each step (force floating point division)
+        steps[0][i] = extent[1]-increment;                      // steps going from high to low
+        steps[1][i] = extent[0]+increment;                      // steps going from low to high
 
-	interval[0]=duration[0]/numSteps;     // low rate interval
-	interval[1]=duration[1]/numSteps;     // high rate interval
+#ifdef _DEBUG
+        Serial.print("low = ");
+        Serial.print(steps[0][i],DEC);
+        Serial.print("   high = ");
+        Serial.println(steps[1][i],DEC);
+#endif
+    }
+
+    interval[0]=duration[0]/numSteps;     // low rate interval
+    interval[1]=duration[1]/numSteps;     // high rate interval
 }
 
 
--- TurnoutServo.h
+++ TurnoutServo.h
@@ -14,10 +14,11 @@
 class TurnoutServo : public Servo
 {
  public:
-	TurnoutServo(byte ServoPin, byte PowerPin);
+    typedef void (*ServoEventHandler)();
+
+    TurnoutServo(byte ServoPin, byte PowerPin);
 	void Initialize(byte ExtentLow, byte ExtentHigh, bool Position);
 	void Initialize(byte ExtentLow, byte ExtentHigh, int DurationLow, int DurationHigh, bool Position);
-	void UpdateOld(unsigned long CurrentMillis);
 	void Update(unsigned long CurrentMillis);
 	void Update();
 	bool IsMoving();
@@ -25,14 +26,13 @@ class TurnoutServo : public Servo
 	void Set(bool Position, bool Rate);
 	void SetExtent(bool Position, byte Extent);
 	void SetDuration(bool Position, int Duration);
-	void SetServoMoveDoneHandler(void (*Handler)());
-	void SetServoPowerOffHandler(void (*Handler)());
-
-private:
-	void ComputeSteps();
+	void SetServoMoveDoneHandler(ServoEventHandler Handler);
+	void SetServoPowerOffHandler(ServoEventHandler Handler);
 
 private:
 	enum ServoState { OFF, STARTING, MOVING, STOPPING };
+
+    void ComputeSteps();
 
 	byte servoPin;                      // pin the servo pwm signal should be sent to
 	byte powerPin;                      // pin used to enable/disable power to the servo
@@ -41,20 +41,21 @@ private:
 	byte steps[2][30];	                // step sequence going each direction
 	byte currentStep = 0;               // counter to track steps in update loop
 	byte extent[2] = { 90, 90 };        // servo angle at LOW and HIGH positions
-	int movementRange;                  // difference between low and high positions, calculated when setting extents
+	int movementRange = 0;              // difference between low and high positions, calculated when setting extents
 	int duration[2] = { 2500, 0 };      // duration (ms) of movement at low and high rates (0 = no delay)
-	int interval[2];					// time interval between each step
-	bool positionSet;                   // the commanded position (high or low) for the servo
-	bool rateSet;                       // the commanded rate of the servo
+    int interval[2] = { 0, 0 };			// time interval between each step
+
+    bool positionSet = 0;               // the commanded position (high or low) for the servo
+	bool rateSet = 0;                   // the commanded rate of the servo
 	ServoState servoState = OFF;        // the current state of the servo
-	unsigned long startTime;            // time at which the servo power will be turned on
-	unsigned long stopTime;	            // time at which the servo power will be turned off
+	unsigned long startTime = 0;        // time at which the servo power will be turned on
+	unsigned long stopTime = 0;	        // time at which the servo power will be turned off
 	const int servoStartDelay = 100;    // delay before powering on the servo after starting pwm signal
 	const int servoStopDelay = 500;     // time the servo remains active after the duration to ensure motion is complete
 	unsigned long lastUpdate = 0;       // time of the last servo write
 
-	void (*servoMoveDoneHandler)();     // pointer to handler for when servo motion is complete
-	void (*servoPowerOffHandler)();     // pointer to handler for when servo power is shut off
+	ServoEventHandler servoMoveDoneHandler = 0;     // pointer to handler for when servo motion is complete
+	ServoEventHandler servoPowerOffHandler = 0;     // pointer to handler for when servo power is shut off
 };
 
 #endif
