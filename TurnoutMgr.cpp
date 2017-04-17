122b74d9ee30a4fd85367a7a7a7dedfa6a2c0923
Eric Thorstenson <eric@thorski.com>
2017-04-17 15:08:51 -0400

    Update turnout manager class to use new bitstream, packet, and decoder libs. Move global event handlers from ino to static methods of this class. Add static reference to the current turnout manager object so that callbacks can access the current instance. Add packet error check and reset of bitstream if needed. Suspend bitstream capture during servo movement.

--- TurnoutMgr.cpp
+++ TurnoutMgr.cpp
@@ -8,16 +8,17 @@
 
 // TurnoutMgr constructor
 TurnoutMgr::TurnoutMgr():
-button(ButtonPin, true),
+    button(ButtonPin, true),
     osStraight(OSstraightPin, true),
     osCurved(OScurvedPin, true),
     led(LedPinR, LedPinG, LedPinB),
     servo(ServoPWMPin, ServoPowerPin),
     relayStraight(RelayStraightPin),
-    relayCurved(RelayCurvedPin)
+    relayCurved(RelayCurvedPin),
+    bitStream(DCCPin, false, 48, 68, 88, 10000, 10),    // bitstream capture object, non-standard timings
+    dccPacket(true, true, 250),                         // DCC packet builder
+    dcc()                                               // DCC packet processor
 {
-    // TODO: why can we initialize member classes here e.g. servo.initialize ?
-    // e.g., why didn't this work in constructor - servo.Initialize(45, 110, LOW);
 }
 
 
@@ -36,19 +37,19 @@ void TurnoutMgr::Initialize()
 }
 
 
-// Run the dcc process update. this needs to be run from loop().
-void TurnoutMgr::UpdateDccProcess()
-{ 
-    dcc.loop();
-}
-
-
-// update the sensors and outputs.
-void TurnoutMgr::UpdateSensors()
+// check for new bitstream data, update sensors and outputs, check for
+// max number of packet errors and reset bitstream capture if necessary
+void TurnoutMgr::Update()
 {
-    unsigned long currentMillis = millis();
+    // if we have new data, go process it
+    if (haveNewBits)
+    {
+        dccPacket.ProcessIncomingBits(bits);     // 120-160 us
+        haveNewBits = false;
+    }
 
     // do the updates to maintain flashing led and slow servo motion
+    unsigned long currentMillis = millis();
     servo.Update(currentMillis);
     led.Update(currentMillis);
 
@@ -56,14 +57,31 @@ void TurnoutMgr::UpdateSensors()
     errorTimer.Update(currentMillis);
     resetTimer.Update(currentMillis);
 
-    // PINB = PINB | (1 << 2);           // timing test output pin 10
-
     // update sensors
     button.Update(currentMillis);
     osStraight.Update(currentMillis);
     osCurved.Update(currentMillis);
 
-    // PINB = PINB | (1 << 3);            // timing test output pin 11
+    // check/reset error counts
+    if (currentMillis - lastMillis > 1000)
+    {
+#ifdef _DEBUG
+        Serial.print("Bit Error Count:  ");
+        Serial.println(bitErrorCount, DEC);
+        Serial.print("Packet Error Count:  ");
+        Serial.println(packetErrorCount, DEC);
+#endif
+        // if we see repeated packet errors, reset bitream capture
+        if (packetErrorCount > maxPacketErrors)
+        {
+            bitStream.Suspend();
+            bitStream.Resume();
+        }
+
+        lastMillis = currentMillis;
+        bitErrorCount = 0;
+        packetErrorCount = 0;
+    }
 }
 
 
@@ -75,10 +93,9 @@ void TurnoutMgr::UpdateSensors()
 // Initialize the turnout manager by setting up the dcc config, reading stored values from CVs, and setting up the servo
 void TurnoutMgr::InitMain()
 {
-    // Configure and initialize the DCC packet processor
-    pinMode(DCCPin, INPUT);
-    byte cv29 = CV29_ACCESSORY_DECODER | CV29_OUTPUT_ADDRESS_MODE;  // configure for acc decoder in output address mode
-    dcc.SetupDecoder(DCCPin, 0, 0, cv29);
+    // Configure and initialize the DCC packet processor (accessory decoder in output address mode)
+    byte cv29 = DCCdecoder::CV29_ACCESSORY_DECODER | DCCdecoder::CV29_OUTPUT_ADDRESS_MODE;
+    dcc.SetupDecoder(0, 0, cv29, false);
 
     // get variables from cv's
     dccAddress = dcc.Address();
@@ -92,7 +109,8 @@ void TurnoutMgr::InitMain()
 
     // servo setup - get extents, rates, and last position from cv's
     State servoState = position;
-    if (servoEndPointSwap) servoState = (State) !servoState;  // swap the servo endpoints if needed
+    if (servoEndPointSwap) 
+        servoState = (servoState == STRAIGHT) ? CURVED : STRAIGHT;  // swap the servo endpoints if needed
     servo.Initialize(
         dcc.GetCV(CV_servoMinTravel),
         dcc.GetCV(CV_servoMaxTravel),
@@ -100,20 +118,36 @@ void TurnoutMgr::InitMain()
         dcc.GetCV(CV_servoHighSpeed) * 100,
         servoState);
 
+    // set pointer to this instance of the turnout manager, so that we can reference it in callbacks
+    currentInstance = this;
+
+    // set callbacks for the bitstream capture
+    bitStream.SetDataFullHandler(WrapperBitStream);
+    bitStream.SetErrorHandler(WrapperBitStreamError);
+
+    // set callbacks for the packet builder
+    dccPacket.SetPacketCompleteHandler(WrapperDCCPacket);
+    dccPacket.SetPacketErrorHandler(WrapperDCCPacketError);
+
     // configure dcc event handlers
-    dcc.SetBasicAccessoryDecoderPacketHandler(&HandleDCCAccPacket, false);
-    dcc.SetBasicAccessoryPomPacketHandler(&HandleDCCAccPomPacket);
+    dcc.SetBasicAccessoryDecoderPacketHandler(WrapperDCCAccPacket);
+    dcc.SetBasicAccessoryPomPacketHandler(WrapperDCCAccPomPacket);
+    dcc.SetExtendedAccessoryDecoderPacketHandler(WrapperDCCExtAccPacket);
+    dcc.SetDecodingErrorHandler(WrapperDCCDecodingError);
 
     // configure other event handlers
-    button.SetButtonPressHandler(&HandleButtonPressWrapper);
-    servo.SetServoMoveDoneHandler(&HandleServoMoveDoneWrapper);
-    servo.SetServoPowerOffHandler(&HandleServoPowerOffWrapper);
-    osStraight.SetButtonPressHandler(&HandleOSStraightWrapper);
-    osCurved.SetButtonPressHandler(&HandleOSCurvedWrapper);
-    errorTimer.SetTimerHandler(&HandleErrorTimerWrapper);
+    button.SetButtonPressHandler(WrapperButtonPress);
+    servo.SetServoMoveDoneHandler(WrapperServoMoveDon);
+    servo.SetServoPowerOffHandler(WrapperServoPowerOff);
+    osStraight.SetButtonPressHandler(WrapperOSStraight);
+    osCurved.SetButtonPressHandler(WrapperOSCurved);
+    errorTimer.SetTimerHandler(WrapperErrorTimer);
 
     // initlize led and relays
     SetRelays();
+
+    // finally, kick off the bitstream capture
+    bitStream.Resume();
 
 #ifdef _DEBUG
     Serial.print("DCC init done, using dcc address ");
@@ -137,7 +171,7 @@ void TurnoutMgr::FactoryReset()
 
     // normal initilization will resume after this timer expires
     resetTimer.StartTimer(resetDelay);
-    resetTimer.SetTimerHandler(&HandleResetTimerWrapper);
+    resetTimer.SetTimerHandler(&WrapperResetTimer);
 
     led.SetLED(RgbLed::MAGENTA, RgbLed::FLASH);
 
@@ -176,13 +210,18 @@ void TurnoutMgr::SetRelays()
 // set the turnout to a new position (also disables relays prior to starting servo motion)
 void TurnoutMgr::SetServo(bool ServoRate)
 {
+    // suspend the bitstream capture, since the servo/led related interrupts cause timing errors in the bitstream.
+    // bitstream capture is resumed in the ServoMoveDoneHandler method.
+    bitStream.Suspend();
+
     // turn off the relays
     relayStraight.SetRelay(LOW);
     relayCurved.SetRelay(LOW);
 
     // set the servo
     State servoState = position;
-    if (servoEndPointSwap) servoState = (State) !servoState;  // swap the servo endpoints if needed
+    if (servoEndPointSwap) 
+        servoState = (servoState == STRAIGHT) ? CURVED : STRAIGHT;  // swap the servo endpoints if needed
     servo.Set(servoState, ServoRate);
 
 #ifdef _DEBUG
@@ -224,7 +263,11 @@ void TurnoutMgr::ServoPowerOffHandler() { }
 
 
 // do things after the servo finishes moving to its new position
-void TurnoutMgr::ServoMoveDoneHandler() { SetRelays(); }
+void TurnoutMgr::ServoMoveDoneHandler() 
+{ 
+    SetRelays();          // set the relays for the new position
+    bitStream.Resume();   // resume the bitstream capture stopped prior to beginning servo motion
+}
 
 
 // handle a button press
@@ -286,15 +329,16 @@ void TurnoutMgr::OSCurvedHandler(bool ButtonState)
 // handle the dcc interpreter callback
 void TurnoutMgr::DCCcommandHandler(unsigned int Addr, unsigned int Direction)
 {
+    // assume we are filtering repeated packets in the packet builder, so we don't check for that here
+
     if (Addr != dccAddress) return;   // exit if this command is not for our address
-    if (dcc.MillisecondsSinceLastPacketToThisDecoder() < 10) return;  // ignore extra commands in 10ms time period
 
     State dccState;
     dccState = (Direction == 0) ? CURVED : STRAIGHT;
     if (dccCommandSwap) dccState = (State) !dccState; // swap the interpretation of dcc command if needed
 
-    // exit if receiving the same dcc command sequentially (cuts down on dcc spam)
-    //if (dccState == position) return;
+    // if we are already in the desired position, just exit
+    if (dccState == position) return;
 
 #ifdef _DEBUG
     Serial.print("In class callback for dcc command direction ");
@@ -323,8 +367,9 @@ void TurnoutMgr::DCCcommandHandler(unsigned int Addr, unsigned int Direction)
 // handle setting CVs
 void TurnoutMgr::DCCPomHandler(unsigned int Addr, byte instType, unsigned int CV, byte Value)
 {
+    // assume we are filtering repeated packets in the packet builder, so we don't check for that here
+
     if (Addr != dccAddress) return;   // exit if this command is not for our address
-    if (dcc.MillisecondsSinceLastPacketToThisDecoder() < 10) return;  // ignore extra commands in 10ms time period
 
 #ifdef _DEBUG
     Serial.print("In class callback for dcc program on main, CV: ");
@@ -335,9 +380,9 @@ void TurnoutMgr::DCCPomHandler(unsigned int Addr, byte instType, unsigned int CV
 
     // set up timer for LED indication, normal led will resume after this timer expires
     errorTimer.StartTimer(2000);
-    errorTimer.SetTimerHandler(&HandleErrorTimerWrapper);
+    errorTimer.SetTimerHandler(&WrapperErrorTimer);
 
-    // verify that the CV is valid
+    // check against our defined CVs to verify that the CV is valid
     boolean isValidCV = false;
     unsigned int numCVs = sizeof(FactoryDefaultCVs)/sizeof(CVPair);
     for (unsigned int i = 0; i < numCVs; i++)
@@ -371,3 +416,81 @@ void TurnoutMgr::DCCPomHandler(unsigned int Addr, byte instType, unsigned int CV
     servo.SetDuration(LOW,dcc.GetCV(CV_servoLowSpeed) * 100);
     servo.SetDuration(HIGH,dcc.GetCV(CV_servoHighSpeed) * 100);
 }
+
+
+
+// callbacks for bitstream and packet builder =============================================================
+
+TurnoutMgr* TurnoutMgr::currentInstance=0;    // pointer to allow us to access member objects from callbacks
+
+// this is called from the ISR for the bitstream capture, so keep it short.
+// we just copy the data and set a flag that it's ready.
+void TurnoutMgr::WrapperBitStream(unsigned long incomingBits) 
+{
+    noInterrupts();   // disable interrupts here, but shouldn't affect next dcc pulse, since this will be right after one
+    currentInstance->bits = incomingBits;
+    interrupts();
+
+    currentInstance->haveNewBits = true;
+}
+
+void TurnoutMgr::WrapperBitStreamError(byte errorCode) 
+{
+    // TODO: add optional LED indication
+    currentInstance->bitErrorCount++;
+}
+
+
+// this is called by the packet builder when a complete packet is ready, to kick off the actual decoding
+void TurnoutMgr::WrapperDCCPacket(byte *packetData, byte size) 
+{
+    // kick off the packet processor
+    currentInstance->dcc.ProcessPacket(packetData, size);
+}
+
+void TurnoutMgr::WrapperDCCPacketError(byte errorCode) 
+{
+    // TODO: add optional LED indication
+    currentInstance->packetErrorCount++;
+}
+
+
+// ========================================================================================================
+// sensor/timer callback wrappers
+
+void TurnoutMgr::WrapperButtonPress(bool ButtonState) { currentInstance->ButtonEventHandler(ButtonState); }
+void TurnoutMgr::WrapperServoMoveDon() { currentInstance->ServoMoveDoneHandler(); }
+void TurnoutMgr::WrapperServoPowerOff() { currentInstance->ServoPowerOffHandler(); }
+void TurnoutMgr::WrapperResetTimer() { currentInstance->ResetTimerHandler(); }
+void TurnoutMgr::WrapperErrorTimer() { currentInstance->ErrorTimerHandler(); }
+void TurnoutMgr::WrapperOSStraight(bool ButtonState) { currentInstance->OSStraightHandler(ButtonState); }
+void TurnoutMgr::WrapperOSCurved(bool ButtonState) { currentInstance->OSCurvedHandler(ButtonState); }
+
+
+// ========================================================================================================
+// dcc processor callback wrappers
+
+void TurnoutMgr::WrapperDCCAccPacket(int boardAddress, int outputAddress, byte activate, byte data)
+{
+    currentInstance->DCCcommandHandler(outputAddress, data);
+}
+
+void TurnoutMgr::WrapperDCCAccPomPacket(int boardAddress,int outputAddress, byte instructionType, int cv, byte data)
+{
+    currentInstance->DCCPomHandler(outputAddress, instructionType, cv, data);
+}
+
+void TurnoutMgr::WrapperDCCExtAccPacket(int boardAddress, int outputAddress, byte data)
+{
+}
+
+void TurnoutMgr::WrapperDCCDecodingError(byte errorCode)
+{
+    // TODO: add optional LED indication
+
+#ifdef _DEBUG
+    Serial.print("Packet error, code: ");
+    Serial.println(errorCode,DEC);
+#endif
+}
+
--- TurnoutMgr.h
+++ TurnoutMgr.h
@@ -9,7 +9,9 @@
 #include "WProgram.h"
 #endif
 
-#include "DCC_Decoder.h"
+#include "Bitstream.h"
+#include "DCCpacket.h"
+#include "DCCdecoder.h"
 #include "TurnoutServo.h"
 #include "RGB_LED.h"
 #include "Button.h"
@@ -20,33 +22,12 @@
 class TurnoutMgr
 {
 public:
-	enum State { STRAIGHT, CURVED };
-
 	TurnoutMgr();
 	void Initialize();
-	void UpdateDccProcess();
-	void UpdateSensors();
-
-	// event handlers
-	void DCCcommandHandler(unsigned int Addr, unsigned int Direction);
-	void DCCPomHandler(unsigned int Addr, byte instType, unsigned int CV, byte Value);
-	void ButtonEventHandler(bool ButtonState);
-	void ServoMoveDoneHandler();
-	void ServoPowerOffHandler();
-	void ResetTimerHandler();
-	void ErrorTimerHandler();
-	void OSStraightHandler(bool ButtonState);
-	void OSCurvedHandler(bool ButtonState);
+	void Update();
 
 private:
-	void InitMain();
-	void FactoryReset();
-	void UpdateOutputs();
-	void SetServo(bool ServoRate);
-	void SetRelays();
-
-private:
-	// Hardware assignments
+    // Hardware assignments
 	const byte DCCPin = 2;
 	const byte ButtonPin = 3;
 	const byte ServoPowerPin = 4;
@@ -59,8 +40,18 @@ private:
 	const byte OSstraightPin = 16;
 	const byte OScurvedPin = 17;
 
-	// Sensors and outputs
-	DCC_Decoder dcc;
+    // main functions
+	void InitMain();
+	void FactoryReset();
+	void SetServo(bool ServoRate);
+	void SetRelays();
+
+    // DCC bitstream and packet processors
+    BitStream bitStream;
+    DCCpacket dccPacket;
+    DCCdecoder dcc;
+
+    // Sensors and outputs
 	Button button;
 	Button osStraight;
 	Button osCurved;
@@ -71,7 +62,16 @@ private:
 	EventTimer resetTimer;
 	EventTimer errorTimer;
 
-	// other instance variables
+    // bitstream and packet builder related
+    boolean haveNewBits = false;          // flag from bitstream object for next set of bits
+    volatile unsigned long bits = 0;      // 32 bits from the bitstream
+    unsigned long bitErrorCount = 0;
+    unsigned long packetErrorCount = 0;
+    const byte maxPacketErrors = 10;      // number of packet errors before bitstream reset
+    unsigned long lastMillis = 0;         // for tracking refresh interval for error counts
+
+    // other instance variables
+    enum State { STRAIGHT, CURVED };
 	byte dccAddress = 1;                       // the dcc address of the decoder
 	State position = STRAIGHT;                 // the current or commanded position of the switch
 	bool servoEndPointSwap = false;            // optionally swap the low/high servo endpoints
@@ -80,7 +80,7 @@ private:
 	bool relaySwap = false;					   // optionally swap the straight/curved relays
 	bool factoryReset = false;                 // is a reset in progress
 
-	// define our cv's
+	// define our available cv's
 	const byte CV_servoMinTravel = 32;
 	const byte CV_servoMaxTravel = 33;
 	const byte CV_servoLowSpeed = 34;
@@ -101,9 +101,9 @@ private:
 	// factory default settings
 	CVPair FactoryDefaultCVs [11] =
 	{
-		{kCV_AddressLSB, 20},    // defined in DCC_Decoder.h, default to address = 1
-		{kCV_AddressMSB, 0},    // defined in DCC_Decoder.h
-		{CV_servoMinTravel, 50},
+		{kCV_AddressLSB, 1},    // defined in DCCdecoder.h, default to address = 1
+		{kCV_AddressMSB, 0},    // defined in DCCdecoder.h
+		{CV_servoMinTravel, 90},
 		{CV_servoMaxTravel, 110},
 		{CV_servoLowSpeed, 25},
 		{CV_servoHighSpeed, 0},
@@ -113,17 +113,42 @@ private:
 		{CV_relaySwap, 0},
 		{CV_turnoutPosition, 0}
 	};
+
+	// event handlers
+	void DCCcommandHandler(unsigned int Addr, unsigned int Direction);
+	void DCCPomHandler(unsigned int Addr, byte instType, unsigned int CV, byte Value);
+	void ButtonEventHandler(bool ButtonState);
+	void ServoMoveDoneHandler();
+	void ServoPowerOffHandler();
+	void ResetTimerHandler();
+	void ErrorTimerHandler();
+	void OSStraightHandler(bool ButtonState);
+	void OSCurvedHandler(bool ButtonState);
+
+    // pointer to allow us to access member objects from callbacks
+    static TurnoutMgr* currentInstance;           
+
+    // callbacks for bitstream and packet builder
+    static void WrapperBitStream(unsigned long incomingBits);
+    static void WrapperBitStreamError(byte errorCode);
+    static void WrapperDCCPacket(byte *packetData, byte size);
+    static void WrapperDCCPacketError(byte errorCode);
+    
+    // Turnout manager event handler wrappers
+    static void WrapperButtonPress(bool ButtonState);
+    static void WrapperServoMoveDon();
+    static void WrapperServoPowerOff();
+    static void WrapperResetTimer();
+    static void WrapperErrorTimer();
+    static void WrapperOSStraight(bool ButtonState);
+    static void WrapperOSCurved(bool ButtonState);
+
+    // DCC event handler wrappers in main
+    static void WrapperDCCAccPacket(int boardAddress, int outputAddress, byte activate, byte data);
+    static void WrapperDCCAccPomPacket(int boardAddress,int outputAddress, byte instructionType, int cv, byte data);
+    static void WrapperDCCExtAccPacket(int boardAddress, int outputAddress, byte data);
+    static void WrapperDCCDecodingError(byte errorCode);
 };
 
-// event handler wrappers in main
-extern void HandleDCCAccPacket(int boardAddress, int outputAddress, byte activate, byte data);
-extern void HandleDCCAccPomPacket(int boardAddress,int outputAddress, byte instructionType, int cv, byte data);
-extern void HandleButtonPressWrapper(bool ButtonState);
-extern void HandleServoMoveDoneWrapper();
-extern void HandleServoPowerOffWrapper();
-extern void HandleResetTimerWrapper();
-extern void HandleErrorTimerWrapper();
-extern void HandleOSStraightWrapper(bool ButtonState);
-extern void HandleOSCurvedWrapper(bool ButtonState);
 
 #endif
