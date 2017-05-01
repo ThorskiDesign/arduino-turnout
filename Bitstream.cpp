998263d50a8e81fab639e980cb23692821548aed
Eric Thorstenson <eric@thorski.com>
2017-05-01 11:42:57 -0400

    Update BitStream class to use SimpleQueue class for capturing DCC interrupt timing. Update to use Timer1 counts rather than micros(). Remove static/volatile except where still needed.

--- Bitstream.cpp
+++ Bitstream.cpp
@@ -1,250 +1,226 @@
 
 #include "Bitstream.h"
 
+//// define/initialize static vars
+//unsigned int BitStream::timeOneMin = DCC_DEFAULT_ONE_MIN * CLOCK_SCALE_FACTOR;
+//unsigned int BitStream::timeOneMax = DCC_DEFAULT_ONE_MAX * CLOCK_SCALE_FACTOR;
+//unsigned int BitStream::timeZeroMin = DCC_DEFAULT_ZERO_MIN * CLOCK_SCALE_FACTOR;
+//unsigned int BitStream::timeZeroMax = DCC_DEFAULT_ZERO_MAX * CLOCK_SCALE_FACTOR;
+//byte BitStream::maxBitErrors = 10;
+//
+//byte BitStream::interruptPin;
+//BitStream::State BitStream::state = SUSPEND;
+//boolean BitStream::lastHalfBit = 0;
+//byte BitStream::bitErrorCount = 0;
+//byte BitStream::queueSize = 0;
+//boolean BitStream::endOfBit = false;
+//
+//unsigned int BitStream::lastInterruptCount = 0;    // 16 bit int for proper overflow with TCNT1
+//unsigned long BitStream::bitData = 0;
+//
+//BitStream::DataFullHandler BitStream::dataFullHandler;
+//BitStream::ErrorHandler BitStream::errorHandler;
+
 
 // define/initialize static vars
-unsigned int BitStream::timeOneMin = 52;
-unsigned int BitStream::timeOneMax = 64;
-unsigned int BitStream::timeZeroMin = 90;
-unsigned int BitStream::timeZeroMax = 10000;   // 110 us for normal bit, 10000 us to allow zero-stretching
-byte BitStream::maxBitErrors = 10;
-
-byte BitStream::interruptPin;
-volatile BitStream::State BitStream::state = SUSPEND;
-volatile boolean BitStream::lastHalfBit = 0;
-volatile boolean BitStream::bitEndsHighOrLow = 0;
-volatile byte BitStream::bitErrorCount = 0;
-volatile byte BitStream::queueSize = 0;
-boolean BitStream::candidateBitEnding;
-byte BitStream::bitEndingMatchCount;
-
-volatile unsigned long BitStream::lastInterruptTime = 0;
-volatile unsigned long BitStream::bitData = 0;
-
-BitStream::DataFullHandler BitStream::dataFullHandler;
-BitStream::ErrorHandler BitStream::errorHandler;
+boolean BitStream::lastPinState = 0;
+SimpleQueue BitStream::simpleQueue;
 
 
 // set up the bitstream capture
 BitStream::BitStream(byte intPin, boolean withPullup)
 {
-    interruptPin = intPin;
+	interruptPin = intPin;
 
-    // configure pins
-    if (withPullup)
-    {
-        pinMode(interruptPin, INPUT_PULLUP);
-    }
-    else
-    {
-        pinMode(interruptPin, INPUT);
-    }
-
-    //// TODO: configure timer2 for use in getting interrupt timing
-    //noInterrupts();
-    //TCCR2A = 0;  // zero the regisers initially
-    //TCCR2B = 0;
-    //TIMSK2 = 0;
-
-    //// configure for 8 prescaler. this gives 0.5 us resolution with a max interval of 127.5 us
-    //// for DCC spec timings, the resulting counter limits are  1: 104 to 128, 0: 180 or greater
-    //TCCR2B |= (1 << 1);   // configure for 8 prescaler
-    //TCNT2 = 0;
-    //interrupts();
+	// configure pins
+	if (withPullup)
+	{
+		pinMode(interruptPin, INPUT_PULLUP);
+	}
+	else
+	{
+		pinMode(interruptPin, INPUT);
+	}
 }
 
 
 // set up the bitstream capture with non-default timings
-BitStream::BitStream(byte interruptPin, boolean withPullup,
-    unsigned int OneMin, unsigned int OneMax, unsigned int ZeroMin, unsigned int ZeroMax, byte MaxErrors)
+BitStream::BitStream(byte interruptPin, boolean withPullup, unsigned int OneMin, unsigned int OneMax,
+	unsigned int ZeroMin, unsigned int ZeroMax, byte MaxErrors) : BitStream(interruptPin, withPullup)
 {
-    timeOneMin = OneMin;
-    timeOneMax = OneMax;
-    timeZeroMin = ZeroMin;
-    timeZeroMax = ZeroMax;
-    maxBitErrors = MaxErrors;
-
-    BitStream(interruptPin, withPullup);
+	timeOneMin = OneMin * CLOCK_SCALE_FACTOR;
+	timeOneMax = OneMax * CLOCK_SCALE_FACTOR;
+	timeZeroMin = ZeroMin * CLOCK_SCALE_FACTOR;
+	timeZeroMax = ZeroMax * CLOCK_SCALE_FACTOR;
+	maxBitErrors = MaxErrors;
 }
 
 
 // set the handler for the data full event
 void BitStream::SetDataFullHandler(DataFullHandler Handler)
 {
-    dataFullHandler = Handler;
+	dataFullHandler = Handler;
 }
 
 
 // set the handler for error events
 void BitStream::SetErrorHandler(ErrorHandler Handler)
 {
-    errorHandler = Handler;
+	errorHandler = Handler;
 }
 
 
 // suspend processing of interrupts
 void BitStream::Suspend()
 {
-    noInterrupts();
-    state = SUSPEND;
-    detachInterrupt(digitalPinToInterrupt(interruptPin));
-    interrupts();
+	noInterrupts();
+	state = SUSPEND;
+	detachInterrupt(digitalPinToInterrupt(interruptPin));
+	interrupts();
 }
 
 
 // begin or resume processing interrupts
 void BitStream::Resume()
 {
-    if (state != SUSPEND) return;   // skip resume if we aren't already suspended
+	if (state != SUSPEND) return;   // skip resume if we aren't already suspended
 
-    noInterrupts();
+	noInterrupts();
 
-    // reset state and bitstream vars
-    state = ACQUIRE;
-    lastHalfBit = 0;
-    bitEndsHighOrLow = 0;
-    bitErrorCount = 0;
+	// Configure timer1 for use in getting interrupt timing, just need a looping counter.
+	// Set up with 8 prescaler. this gives 0.5 us resolution with an overflow every 32.7ms.
+	TCCR1A = 0;    // reset the registers initially
+	TCCR1B = 0;
+	TCCR1C = 0;
+	TCNT1 = 0;
+	TIMSK1 = 0;
+	TCCR1B |= (1 << 1);   // set CS11 bit for 8 prescaler
 
-    // initialize the queue
-    queueSize = 0;
-    bitData = 0;
+	// reset state and bitstream vars
+	state = NORMAL;
+	lastHalfBit = 0;
+	endOfBit = false;
+	bitErrorCount = 0;
+	simpleQueue.Reset();    // reset the queue of DCC timestamps
 
-    // set starting time and attach interrupt
-    lastInterruptTime = micros();
-    attachInterrupt(digitalPinToInterrupt(interruptPin), HandleInterrupt, CHANGE);
+	// initialize the outgoing queue
+	queueSize = 0;
+	bitData = 0;
 
-    interrupts();
+	// set starting time and attach interrupt
+	lastInterruptCount = TCNT1;
+	attachInterrupt(digitalPinToInterrupt(interruptPin), GetIrqTimestamp, CHANGE);
+
+	interrupts();
+}
+
+
+// process the queued DCC timestamps to see if they represent a one or a zero.
+void BitStream::ProcessTimestamps()
+{
+	//if (simpleQueue.Size()>0) HW_DEBUG_PULSE();
+	//if (simpleQueue.Size()>1) HW_DEBUG_PULSE();
+	//if (simpleQueue.Size()>2) HW_DEBUG_PULSE();
+	//if (simpleQueue.Size()>3) HW_DEBUG_PULSE();
+	//if (simpleQueue.Size()>4) HW_DEBUG_PULSE();
+	//if (simpleQueue.Size()>5) HW_DEBUG_PULSE();
+
+	while (simpleQueue.Size() > 0)
+	{
+		// get the current timestamp to check
+		unsigned int currentCount = simpleQueue.Get();
+
+		// get the period between the current and last timestamps
+		unsigned int period = currentCount - lastInterruptCount;
+
+		// does the period give a 1 or a 0?
+		boolean isOne = (period >= timeOneMin && period <= timeOneMax);
+		boolean isZero = (period >= timeZeroMin && period <= timeZeroMax);
+
+		// check for valid half bit
+		if (isOne || isZero)
+		{
+			// now we have validated we have a 1 or 0, so isOne represents our current half bit
+
+			// check if the current and previous half bits match
+			if (isOne == lastHalfBit)
+			{
+				if (endOfBit)             // is this the ending half bit?
+				{
+					QueuePut(isOne);      // add the bit to the queue
+					endOfBit = false;
+					bitErrorCount = 0;    // reset error count after full valid bit
+				}
+				else
+				{
+					endOfBit = true;      // this wasn't the ending half bit, so the next one will be
+				}
+			}
+			else
+			{
+				endOfBit = true;   // transitioned from 1 to 0 or vice versa, so the next half bit is the bit end
+			}
+
+			// save the time of the last valid interrupt and half bit
+			lastInterruptCount = currentCount;
+			lastHalfBit = isOne;
+		}
+		else    // didn't get a valid half bit, so begin error handling
+		{
+			// callback error handler
+			if (errorHandler)
+				errorHandler(ERR_INVALID_HALF_BIT);
+
+			bitErrorCount++;        // increment error count
+			if (bitErrorCount > maxBitErrors)
+			{
+				// normally assume an error is an intermittent event, and don't reset lastInterrupt time,
+				// so that the next 'real' interrupt still has a valid starting point.
+				// but, if we have > maxBitErros, assume something bad happened and set it here,
+				// so that the subsequent period is based on the latest irq time.
+				lastInterruptCount = currentCount;
+				simpleQueue.Reset();
+
+				// callback error handler
+				if (errorHandler)
+					errorHandler(ERR_SEQUENTIAL_ERROR_LIMIT);
+			}
+		}
+
+		// 6 - 8 us per timestamp
+
+	}     // end while
 }
 
 
 // add a bit to the queue, performing callback and reset if full
-void BitStream::QueuePut(boolean newBit)   // static
+void BitStream::QueuePut(boolean newBit)
 {
-    // method executes in ~2 us, unless we are at max queue size, which adds ~3us.
+	// method executes in ~2 us, unless we are at max queue size, which adds ~3us.
 
-    // shift the previous bits left and add the new bit
-    bitData = (bitData << 1) + newBit;    // approx 1.5 us
+	// shift the previous bits left and add the new bit
+	bitData = (bitData << 1) + newBit;    // approx 1.5 us
 
-    // perform callback and reset if queue is full
-    // this adds about 3us when we hit it
-    queueSize++;
-    if (queueSize > maxBitIndex)
-    {
-        if (dataFullHandler)
-            dataFullHandler(bitData);
-        queueSize = 0;
-        bitData = 0;
-    }
+	// perform callback and reset if queue is full
+	// this adds about 3us when we hit it
+	queueSize++;
+	if (queueSize > maxBitIndex)
+	{
+		if (dataFullHandler)
+			dataFullHandler(bitData);
+		queueSize = 0;
+		bitData = 0;
+	}
 }
 
 
-// process the interrupts
-void BitStream::HandleInterrupt()   // static
+void BitStream::GetIrqTimestamp()    // static
 {
-    // get entry time and period
-    unsigned long irqTime = micros();    // this takes ~3 us, only has 4 us resolution
-    unsigned long period = irqTime - lastInterruptTime;
+	boolean pinState = HW_IRQ_PORT();                 // this takes ~0.2 us
+	if (pinState == lastPinState) return;
+	lastPinState = pinState;
 
-    // compute one or zero
-    boolean isOne = (period >= timeOneMin && period <= timeOneMax);
-    boolean isZero = (period >= timeZeroMin && period <= timeZeroMax);
+	simpleQueue.Put(TCNT1);
 
-    // check for invalid half bit
-    if (!isOne && !isZero)
-    {
-        // callback error handler
-        if (errorHandler)
-            errorHandler(ERR_INVALID_HALF_BIT);
-
-        bitErrorCount++;        // increment error count
-        if (bitErrorCount > maxBitErrors)
-        {
-            // normally we assume an error is an intermittent event, and don't reset lastInterrupt time,
-            // so that the next 'real' interrupt still has a valid starting point.
-            // but, if we have > maxBitErros, assume something bad happened and set it here,
-            // so that the subsequent acquire period is based on the latest irq time.
-            lastInterruptTime = irqTime;
-            state = ACQUIRE;
-
-            // callback error handler
-            if (errorHandler)
-                errorHandler(ERR_SEQUENTIAL_ERROR_LIMIT);
-        }
-
-        return;   // no further processing without a valid one our zero
-    }
-
-    // at this point, we have validated we have a 1 or 0, so isOne represents our current half bit
-
-    // perform the state operations
-    switch (state)
-    {
-    case ACQUIRE:
-        {
-            // look for a one halfbit followed by a zero halfbit
-            if (lastHalfBit == 1 && isOne == 0)
-            {
-                // if this is our first acquisition, get the candidate bit ending
-                if (bitEndingMatchCount == 0)
-                {
-                    // get current pin state. since we check this after the irq that ends the
-                    // zero half bit, this represents the state that the bits will end on
-                    candidateBitEnding = (HW_IRQ_PORT()) ? 1 : 0;
-
-                    bitEndingMatchCount++;   // not really a match, but start the next steps
-                }
-                else     // subsequent acquisitions
-                {
-                    // if we have no errors and the next bit end matches the candidate bit end
-                    boolean currentPortReading = (HW_IRQ_PORT()) ? 1 : 0;
-                    if (bitErrorCount == 0 && currentPortReading == candidateBitEnding)
-                    {
-                        if (bitEndingMatchCount < bitEndingMinimumMatches)    // still need more matches
-                        {
-                            bitEndingMatchCount++;
-                        }
-                        else      // final matching acquisition
-                        {
-                            // we have validated the candidata bit ending, so assign it
-                            bitEndsHighOrLow = candidateBitEnding;
-                            bitEndingMatchCount = 0;
-
-                            // begin normal processing
-                            state = NORMAL;
-                            bitErrorCount = 0;
-                        }
-                    }
-                    else    // we had an intervening error or a non-match, start over
-                    {
-                        bitEndingMatchCount = 0;
-                        bitErrorCount = 0;
-                    }
-                }
-            }
-            break;
-        }
-
-    case NORMAL:
-        {
-            // get current pin state
-            boolean pinState = HW_IRQ_PORT();                 // this takes ~0.2 us
-            //boolean pinState = digitalRead(interruptPin);   // this takes ~2.5 us
-
-            // check if the current and previous half bits match and we're at the ending half bit.
-            // note check of pinState is inverted since we are getting the pinState after the
-            // irq that makes up the end of the bit.
-            if ((isOne == lastHalfBit) && (pinState != bitEndsHighOrLow))
-            {
-                QueuePut(isOne);      // add the bit to the queue
-                bitErrorCount = 0;    // reset error count after full valid bit
-            }
-            break;
-        }
-    }
-
-    // save the time of the last valid interrupt and half bit
-    lastInterruptTime = irqTime;
-    lastHalfBit = isOne;
-
-    // approx 7-11 us to complete, using direct port reads
+	// 2.5 microseconds, with pin state check, to add new timestamp to queue
 }
--- Bitstream.h
+++ Bitstream.h
@@ -9,9 +9,11 @@ http://www.nmra.org/sites/default/files/standards/sandrp/pdf/s-9.1_electrical_st
 Summary:
 
 A hardware interrupt is configured on CHANGE, so each time the signal transitions, the ISR is called.
-The time between the current interrupt and the previous interrupt is used to determine whether the
-current half of the bit indicates a 0 or a 1. After a valid bit is found, it is added to the queue.
-When the queue is full, a callback is performed to provide the data.
+In the ISR, the time of the interrupt in Timer1 counts is stored in a queue as an unsigned int.
+When the timestamps are retrieved from the queue, the time between the current timestamp and the 
+previous timestamp is used to determine whether the current half of the bit indicates a 0 or a 1. 
+After a valid bit is found, it is added to an output queue. When the output queue is full, a 
+callback is performed to provide the data.
 
 Example usage:
 
@@ -19,40 +21,35 @@ Example usage:
     bitStream(DCCPin, false);                           // bitstream capture object, default timings
     bitStream(DCCPin, false, 48, 68, 88, 10000, 10);    // bitstream capture object, non-standard timings
     bitStream.Resume();                                 // start the bitstream capture
+	bitStream.Suspend();                                // stop the bitstream capture
+	bitStream.ProcessTimeStamps();					    // process any DCC timestamps in the queue
 
 Details:
 
-The class operates in two main states, ACQUIRE and NORMAL. In the ACQUIRE mode, the bitstream is 
-examined for a 1 half bit, followed by a zero half bit, which allows determination of whether the 
-bit pairs end high or low (depending on the polarity of the DCC signal input). A configurable number
-of error-free 1-to-0 transitions are required to avoid incorrectly detecting a single spurious event.
-After the final transision is found, the bit ending (high or low) is assigned, and NORMAL operation
-begins.
-
-In the NORMAL mode, the bitstream is examined for a matching pair of high and low half bits that
-makes up a full bit. When a complete bit is found, it is added to the queue.
-
-Error checking is performed on each half bit. If a half bit does not fall within the valid ranges for
-a 0 or 1, a callback is triggered and a counter incremented. After a configurable number of 
-consecutive bit errors, a callback is triggered and operation falls back to the ACQUIRE mode. The
-bit error count is reset after a successful acquisition, and after each complete bit in NORMAL mode.
+In the NORMAL mode, the ISR is attached and captures timestamps of the DCC events in a queue.
+The timestamp queue is examined for a matching pair of half bits that makes up a full bit. When 
+a complete bit is found, it is added to the output queue. Error checking is performed on each half 
+bit. If a half bit does not fall within the valid ranges for a 0 or 1, a callback is triggered 
+and a counter incremented. After a configurable number of consecutive bit errors, another callback 
+is triggered and the timestamp queue is reset. The bit error count is reset after each complete bit.
 
 Suspend/Resume methods allow starting, stopping, or resetting the bitstream capture, depending
 on outside factors (for example, during times when the signal may be degraded, or when other higher
 priority processing needs to take place). The hardware interrupt is detached when suspended, and
-attached when resumed.
+attached when resumed. Timer1 is configured in the Resume method, so that it can be used for other
+purposes (e.g. servo) when the bitstream capture is suspended.
 
-The queue is an unsigned long, into which 32 bits are stored as they are received. The queue is
+The output queue is an unsigned long, into which 32 bits are stored as they are received. The queue is
 shifted left each time a bit is added, so the bits are stored left to right in the order in which 
 the are received. After 32 bits have been stored, a callback is triggered, and the queue is reset.
 
-Notes on hardware timing: The ISR in NORMAL mode takes between 7 and 11 microseconds to run. The
-micros() call makes up about 3 of those, and only has a resolution of 4 us. Additionally, any other
-interrupts will affect the timing of this interrupt. The 0/1 timing limits default to the DCC spec,
-however wider limits will result in reduced bit errors if other interrupts are occuring.
-
-TODO: revise the interrupt timing to use a dedicated timer read (e.g., TIMER2) rather than the
-micros call, to provide better resolution and reduce the time in the ISR.
+Notes on hardware timing: The ISR in NORMAL mode takes 2.5 microseconds to run, so any pulses faster
+than that will get lost or have questionable timestamps. Additionally, other interrupts may affect 
+the timing of this interrupt. The 0/1 timing limits default to the DCC spec, however wider limits 
+may result in reduced bit errors if other interrupts are occuring. Timer1 is configured with a prescaler
+of 8, which gives 0.5 microsend resolution, with an overflow every ~32 milliseconds. The timestamp 
+queue, period calculations, etc. are done using unsigned int so that the overflow is handled
+transparently.
 
 */
 
@@ -67,6 +64,9 @@ micros call, to provide better resolution and reduce the time in the ISR.
 #endif
 
 
+#include "SimpleQueue.h"
+
+
 // defines for direct port access and hardware debugging pulses
 #define HW_IRQ_PORT() PIND & 0x04                                                    // direct access h/w port pin 2
 #define HW_DEBUG_PULSE() { PORTB = PORTB | (1 << 4); PORTB = PORTB & ~(1 << 4); }    // pulse pin 12
@@ -75,6 +75,14 @@ micros call, to provide better resolution and reduce the time in the ISR.
 
 #define ERR_INVALID_HALF_BIT             1
 #define ERR_SEQUENTIAL_ERROR_LIMIT       2
+
+#define DCC_DEFAULT_ONE_MIN				52
+#define DCC_DEFAULT_ONE_MAX				64
+#define DCC_DEFAULT_ZERO_MIN			90
+#define DCC_DEFAULT_ZERO_MAX			10000    // 110 us for normal bit, 10000 us to allow zero-stretching
+
+#define CLOCK_SCALE_FACTOR				2        // number of clock ticks per microsecond
+                                                 // 8 prescaler gives a 0.5 us interval
 
 
 class BitStream
@@ -98,47 +106,46 @@ public:
     void Suspend();
     void Resume();
 
+	// process the raw timestamp queue
+	void ProcessTimestamps();
+
 private:
     // states
     enum State
     {
-        ACQUIRE,
         NORMAL,
         SUSPEND
     };
 
     // DCC microsecond 0 & 1 timings
-    static unsigned int timeOneMin;
-    static unsigned int timeOneMax;
-    static unsigned int timeZeroMin;
-    static unsigned int timeZeroMax;          // 110 us for normal bit, 10000 us to allow zero-stretching
-    static byte maxBitErrors;                 // max number of bit errors before we revert to acquire state
+    unsigned int timeOneMin = DCC_DEFAULT_ONE_MIN * CLOCK_SCALE_FACTOR;
+    unsigned int timeOneMax = DCC_DEFAULT_ONE_MAX * CLOCK_SCALE_FACTOR;
+    unsigned int timeZeroMin = DCC_DEFAULT_ZERO_MIN * CLOCK_SCALE_FACTOR;
+    unsigned int timeZeroMax = DCC_DEFAULT_ZERO_MAX * CLOCK_SCALE_FACTOR;
 
     // Event handlers
-    static DataFullHandler dataFullHandler;        // handler for the data full event
-    static ErrorHandler errorHandler;              // handler for errors
+    DataFullHandler dataFullHandler = 0;    // handler for the data full event
+    ErrorHandler errorHandler = 0;          // handler for errors
 
     // Interrupt and state variables
-    static byte interruptPin;                               // the pin for the hardware irq
-    volatile static unsigned long lastInterruptTime;        // time of last interrupt
-    volatile static boolean lastHalfBit;                    // the last half bit captured
-    volatile static boolean bitEndsHighOrLow;               // do the bits end with a high or low half bit
-    volatile static State state;                            // current state of the acquisition
-    volatile static byte bitErrorCount;                     // current number of sequential bit errors
-    static boolean candidateBitEnding;             // first identified bit ending to be verified
-    static byte bitEndingMatchCount;               // number of matching bit endings recorded
-    static const byte bitEndingMinimumMatches = 10;         // number of matches required before we consider a 
-                                                            // canditate bit ending valid
+    byte interruptPin;                      // the pin for the hardware irq
+    State state = SUSPEND;                  // current state of the acquisition
+	unsigned int lastInterruptCount = 0;    // Timer1 count at the last interrupt
+    boolean lastHalfBit = 0;                // the last half bit captured
+	boolean endOfBit = false;               // second half-bit indicator
+    byte bitErrorCount = 0;                 // current number of sequential bit errors
+    byte maxBitErrors = 10;                 // max number of bit errors before we revert to acquire state
+	static boolean lastPinState;            // last state of the IRQ pin
+	static SimpleQueue simpleQueue;         // queue for the DCC timestamps
 
-    // Queue structure
-    static const byte maxBitIndex = 31;            // 32 bits total to store in unsigned long
-    volatile static byte queueSize;                // current size of the queue
-    volatile static unsigned long bitData;         // stores the bitstream
+    // Output queue structure
+    const byte maxBitIndex = 31;            // 32 bits total to store in unsigned long
+    byte queueSize = 0;                     // current size of the queue
+    unsigned long bitData = 0;              // stores the bitstream
 
     // private methods
-    static void HandleInterrupt();                 // handles the hardware interrupt
-    static void QueuePut(boolean newBit);          // adds a bit to the queue
+    void QueuePut(boolean newBit);          // adds a bit to the queue
+	static void GetIrqTimestamp();		    // get and queue the timestamp of an interrupt
 };
 
 #endif
-
