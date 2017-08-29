407e74bc75b94a7d0e52c05ffd73421c20dcc7eb
Eric Thorstenson <eric@thorski.com>
2017-08-29 13:39:38 -0400

    Add empty class for turnout manager base.

---
+++ TurnoutBase.h
@@ -0,0 +1,14 @@
+// TurnoutBase.h
+
+#ifndef _TURNOUTBASE_h
+#define _TURNOUTBASE_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
